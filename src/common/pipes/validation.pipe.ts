a1fc95d0a84f6a57d276fe54914c25366919c6cc
KIM MIN HO <minmin0416@khu.ac.kr>
2021-08-07 01:25:12 +0900

    feat: Global한 ValidationPipe 추가
    ExceptionError를 handle 하는 global한 ValidationPipe를 추가한다.
    Validation을 위한 npm class-validator, class-transformer 추가한다.
    
    Resolves: #3

---
+++ src/common/pipes/validation.pipe.ts
@@ -0,0 +1,32 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      throw new BadRequestException(
+        errors[0].constraints[Object.keys(errors[0].constraints)[0]],
+        `${metatype.name} ${JSON.stringify(errors[0].property)} validate 오류.`,
+      );
+    }
+
+    return object;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
