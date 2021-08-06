8f35fb5bb48b40811bc7b293e22f2bce0820d9aa
KIM MIN HO <minmin0416@khu.ac.kr>
2021-08-07 00:31:59 +0900

    feat: Config 모듈 추가 및 도입
    npm @nestjs/config 모듈을 도입한다.
    .env, .env.staging, .env.production 파일의 변수를 읽어 환경변수로 가져오며,
    현재는 PORT의 환경 변수를 추가해 서버의 포트를 설정한다.
    
    추가로 @nestjs/config 빌드 시 typescript의 버전 문제로 인해 typescript 버전을 업그레이드 했다.
    npm @nestjs/config, typescript가 프로젝트에 영향을 받는다.
    
    Resolves: #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? join(__dirname, '../.env.production')
+          : process.env.NODE_ENV === 'staging'
+          ? join(__dirname, '../.env.staging')
+          : join(__dirname, '../.env'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { AppConfig } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<AppConfig>('app').port;
+  await app.listen(port);
 }
 bootstrap();
